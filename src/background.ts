b3577327610dff6eed8899087673e3e5e3993949
devlopersabbir <devlopersabbir@gmail.com>
2023-11-19 23:36:14 +0600

    create reload

--- src/background.ts
+++ src/background.ts
@@ -4,4 +4,23 @@ browser.runtime.onInstalled.addListener((details) => {
   console.log("Extension installed:", details);
 });
 
-const tabIdList: any = {};
+let tabIdList: any = [];
+
+browser.tabs.onUpdated.addListener(
+  (tabId: number, changeInfo: browser.Tabs.OnUpdatedChangeInfoType) => {
+    tabIdList = [...tabIdList, tabId];
+    // console.log("tab id: ", tabId);
+    console.log("list of tab: ", tabIdList);
+  }
+);
+console.log("list of tab: ", tabIdList);
+
+// **** Create Tab Reload *** //
+const createReload = async () => {
+  const tabs = await browser.tabs.query({
+    active: true,
+    currentWindow: true,
+    windowType: "normal",
+  });
+  console.log(tabs);
+};
